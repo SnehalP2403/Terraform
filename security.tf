5dbd76e73eea058a425e36a6000fe10b783757b4
Snehal Pandare <132730597+SnehalP2403@users.noreply.github.com>
2023-08-28 15:00:06 +0530

    Create security.tf

---
+++ security.tf
@@ -0,0 +1,17 @@
+resource "aws_security_group" "Security_group" {
+  name_prefix = "SSH-GG"
+  
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
